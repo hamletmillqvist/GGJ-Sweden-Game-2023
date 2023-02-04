6a314a13914963b2ba5ffdbcb00a7a52789d482a
ErikLNH <erikh99@gmail.com>
2023-02-05 00:53:28 +0100

    Main menu added

---
+++ GGJSGA23/Assets/MenuManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField]
+    private Canvas mainMenuCanvas;
+    [SerializeField]
+    private Canvas creditsCanvas;
+    private int activeScene = 0;
+    //bool changingScene = false;
+    //Scene mainScene = SceneManager.GetSceneByName("SampleScene");
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void StartGame()
+    {
+        //changingScene = true;
+        //activeScene++;
+        
+        //SceneManager.LoadScene(activeScene, LoadSceneMode.Single);
+        SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+    }
+    public void Exitgame()
+    {
+        Application.Quit();
+    }
+    public void ShowCredits()
+    {
+        creditsCanvas.gameObject.SetActive(true);
+        mainMenuCanvas.gameObject.SetActive(false);
+    }
+    public void GoBack()
+    {
+        creditsCanvas.gameObject.SetActive(false);
+        mainMenuCanvas.gameObject.SetActive(true);
+    }
+}
