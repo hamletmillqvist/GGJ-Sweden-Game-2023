bad07530f7d9e2c38daa8d3043f07506dcf9e6e2
hamletmillqvist <valrossen.oliver@protonmail.ch>
2023-02-04 15:21:37 +0100

    Added item spawner behaviour

---
+++ GGJSGA23/Assets/Scripts/RootRacer/SpawnedItemBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace RootRacer
+{
+	[RequireComponent(typeof(CircleCollider2D))]
+	public class SpawnedItemBehaviour : MonoBehaviour
+	{
+		private CircleCollider2D circleCollider2D;
+
+		public float Radius => circleCollider2D.radius;
+		
+		private void Awake()
+		{
+			circleCollider2D = GetComponent<CircleCollider2D>();
+		}
+
+		private void OnDestroy()
+		{
+			ItemSpawnerBehaviour.UnsubscribeSpawnedItem(this);
+		}
+
+		public bool GetIsInsideRadius(Vector2 point) => circleCollider2D.OverlapPoint(point);
+	}
+}
